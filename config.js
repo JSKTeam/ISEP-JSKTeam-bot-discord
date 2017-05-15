059eadd048bd6622ea21dac02d73ee18cefb8b81
katiafiroud <katia_firoud@hotmail.fr>
2017-05-15 16:35:39 +0200

    Changement Token BOT_DISCORD

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
+
   token: 'MzEzNjUwODM2NzMzNjg5ODU2.DARgwg.09hZ6K0o1GBReimg8PXIBVRL3nw',
-  channel: '306031162415906816'
+  channel: '306031162415906816',
+  OpenWeatherMap: 'f82ca33a6cc024fc5ccdbac8adea377b'
 }
