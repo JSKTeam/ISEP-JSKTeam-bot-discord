d89e042ff21f11d14f977938cd957ae3e33a81a1
katiafiroud <katia_firoud@hotmail.fr>
2017-06-13 01:44:36 +0200

    test  Please enter the commit message for your changes. Lines starting

--- tests/example.js
+++ tests/example.js
@@ -37,14 +37,19 @@ test('test pokemon', t => {
     })
 })
 
+
+
 test('test traduction', t => {
-  return client.PostPromise('https://translation.googleapis.com/language/translate/v2')
-    .catch((error) => {
-      t.fail()
-      throw error
-    })
-    .then((res) => {
-      console.log(res.response.statusCode)
-      t.is(res.response.statusCode, 200)
-    })
+  var args = {
+    parameters: {'q': 'Bonjour', 'target': 'en', 'format': 'text', 'key': 'AIzaSyDtM-DG6jVikfCI1MUrw31VEbvFm5Ifezw'}
+  }
+  return client.postPromise('https://translation.googleapis.com/language/translate/v2', args)
+  .catch((error) => {
+    t.fail()
+    throw error
+  })
+  .then((res) => {
+    console.log(res.response.statusCode)
+    t.is(res.response.statusCode, 200)
+  })
 })
